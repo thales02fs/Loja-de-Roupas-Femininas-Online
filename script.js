415b061ebfe282406f57d6054fb5ebe00852524b
thales02fs <thalesfreitas026@gmail.com>
2025-07-11 23:41:20 -0300

    Removi o arquivo js

--- script.js
+++
@@ -1,9 +0,0 @@
-const divCarrossel = document.querySelector('.div-carrossel');
-let cardsCarrossel = document.querySelectorAll('.cards-carrossel');
-let card1 = document.querySelectorAll('#card1');
-let card2 = document.querySelectorAll('#card2');
-let card3 = document.querySelectorAll('#card3');
-let esquerda = document.querySelector('.esquerda');
-let meio = document.querySelector('.meio');
-let direita = document.querySelector('.direita');
-
